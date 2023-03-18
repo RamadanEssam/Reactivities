f269750163cd013509bd511bd32841313910a929
RamadanEssam <Ramadaneita@gmail.com>
2023-03-18 16:34:34 +0200

    Add project files.

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")] // loacl
+    public class BaseApiController : ControllerBase
+    {
+       
+    }
+}
+ 
---
+++ API/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context=services.GetRequiredService<DbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+
+}
+catch (Exception ex)
+{
+
+	var logger =services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "an error occerd during migration");
+}
+
+app.Run();
